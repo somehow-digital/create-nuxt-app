539de709da4f7bbf5fa714eae6904be5cf5f1c12
Thomas Deinhamer <code@thasmo.dev>
2024-11-15 17:47:50 +0100

    Remove storybook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
-	modules: ['@nuxt/eslint', '@nuxtjs/storybook'],
+	modules: ['@nuxt/eslint'],
 });
