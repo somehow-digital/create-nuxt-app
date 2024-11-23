014971a7680cc8375d5b21b9e86d1c5bbcd79620
Thomas Deinhamer <184284+thasmo@users.noreply.github.com>
2024-11-23 18:30:11 +0100

    chore: finalize setup (#22)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-import config from '@somehow-digital/eslint-config/typescript';
-
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-// todo: merge config properly
-export default withNuxt(...config);
+export default withNuxt();
